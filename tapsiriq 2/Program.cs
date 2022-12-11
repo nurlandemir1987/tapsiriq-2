5c5550018ff84bab80989662105a31872c621832
nurlandemir1987 <nurlannd@code.edu.az>
2022-12-11 16:06:29 +0400

    Add project files.

---
+++ tapsiriq 2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace tapsiriq_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("birinci reqem :");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("ikinci reqem :");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write($"{a} ededinin / {b} ededine bolunmesinden alinan qaliq = {a % b}");
+        }   
+    }
+}
